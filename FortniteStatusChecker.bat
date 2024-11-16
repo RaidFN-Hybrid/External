55d77f67d8ec14ff89b878d0b74e58eafd8a83b6
RaidFN <foxonhybrid@gmail.com>
2024-11-16 04:06:39 -0500

    Update FortniteStatusChecker.bat

--- FortniteStatusChecker.bat
+++ FortniteStatusChecker.bat
@@ -4,13 +4,9 @@ color 1
 
 title Fortnite Status Checker
 
-:showlogs
+:logs
 
 type %localappdata%\FortniteGame\Saved\Logs\FortniteGame.log
-
-goto showlogs
-
-:disableproxy
 
 tasklist /fi "IMAGENAME eq FortniteLauncher.exe" 2>NUL | find /i "FortniteLauncher.exe" >nul
 
@@ -18,4 +14,4 @@ if errorlevel 1 (
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul
 )
 
-goto disableproxy
+goto logs
