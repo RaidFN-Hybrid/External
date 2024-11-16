7adaacd23ba625a48ae52d5ba8d484e894690f0d
RaidFN <foxonhybrid@gmail.com>
2024-11-15 21:50:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1044,7 +1044,7 @@ class RaidFN:
           continue
 
       async with aiohttp.ClientSession() as session:
-        eac_splash = "https://i.ibb.co/DwWCZ2w/Splash-Screen.png"
+        eac_splash = "https://i.ibb.co/GnKRxSG/Splash-Screen.png"
         async with session.get(eac_splash) as request:
           content = await request.read()
       
