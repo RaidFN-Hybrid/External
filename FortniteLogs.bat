c80afebf845bf308046db3b4ee3c041159ece78c
RaidFN <foxonhybrid@gmail.com>
2024-11-16 04:16:47 -0500

    Update FortniteLogs.bat

--- FortniteLogs.bat
+++ FortniteLogs.bat
@@ -4,11 +4,15 @@ color 1
 
 title Fortnite Logs
 
-:logs
+:showlogs
 
 timeout /t 60 /nobreak >nul
 
 type %localappdata%\FortniteGame\Saved\Logs\FortniteGame.log
+
+goto showlogs
+
+:disableproxy
 
 tasklist /fi "IMAGENAME eq FortniteLauncher.exe" 2>NUL | find /i "FortniteLauncher.exe" >nul
 
@@ -16,4 +20,4 @@ if errorlevel 1 (
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul
 )
 
-goto logs
+goto disableproxy
