f7986baae9029fe8b9bf0c1e59885a576448b20e
RaidFN <foxonhybrid@gmail.com>
2024-11-16 02:49:02 -0500

    Add files via upload

---
+++ FortniteChecker.bat
@@ -0,0 +1,19 @@
+@echo off
+
+color 1
+
+title Fortnite Checker
+
+wscript.exe "%~dp0invisible.vbs" "%~dp0FortniteChecker.bat"
+
+:loop
+
+tasklist /fi "IMAGENAME eq FortniteLauncher.exe" 2>NUL | find /i "FortniteLauncher.exe" >nul
+
+if errorlevel 1 (
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul
+
+taskkill /im WindowsTerminal.exe
+)
+
+goto loop
