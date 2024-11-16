0bab0ee5b41d00ec00cdb10c2b1a03e9bf3489eb
RaidFN <foxonhybrid@gmail.com>
2024-11-16 03:09:55 -0500

    Update FortniteStatusChecker.bat

--- FortniteStatusChecker.bat
+++ FortniteStatusChecker.bat
@@ -12,6 +12,4 @@ if errorlevel 1 (
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f >nul
 )
 
-taskkill /im WindowsTerminal.exe
-
 goto loop
